860c8fde707f428add648c841e69914ed2c65461
Tom Carfano <tomcarfano@TC-Macbook-Pro.lan>
2023-08-09 22:12:41 -0700

    fix query owner section

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const app = express();
 const pets = require('./pets')
 
 let arrayNames = [];
-let arrayOwners = [];
 
+//Just a title for the homepage
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
 //Loop through pets array and make a new array just containing names
-//if user input 
 app.get('/pets', (req, res) => {    
     const mapPets = () => {
         for(let i = 0; i < pets.length; i++){
@@ -21,25 +20,37 @@ app.get('/pets', (req, res) => {
     res.send(arrayNames.join(', '))
 })
 
+//If user navigates straight to owner display first pet name
+app.get('/pets/owner', (req, res) => {
+
+  const getOwnersPets = () => {
+    for(let i = 0; i < pets.length; i++){
+        if(req.query.ownerName === pets[i].owner){
+            res.send(`Hello: ${pets[i].name}`)
+        }
+    }
+  }
+  getOwnersPets()
+})
+
+//If user types in a pet name welcome the pet
 app.get('/pets/:name', (req, res) => {
         if(arrayNames.includes(req.params.name)){
             res.send(`Welcome ${req.params.name}`)
-        } else {
+            //If user first types in a name then navigates to owner use this function to display pet name
+        } else if(req.params.name.includes('owner')){
+
+                const getOwnersPets = () => {
+                  for(let i = 0; i < pets.length; i++){
+                      if(req.query.ownerName === pets[i].owner){
+                          res.send(`Hello: ${pets[i].name}`)
+                      }
+                  }
+                }
+                getOwnersPets()
+        } else (
             res.send("No pet by that name")
-        }
-})
-
-app.get('/pets/owner/:owner', (req, res) => {
-    for(let i = 0; i < pets.length; i++){
-        arrayOwners.push(pets[i].owner)
-    }
-    console.log(arrayOwners)
-
-    if(arrayOwners.includes(req.params.owner)){
-        res.send(`Welcome ${req.params.owner}`)
-    } else {
-        res.send("No Owner by that name")
-    }
+        )
 })
 
 const PORT = 8000;
